48217e90ae22e3d0388c17815cad2deaabed5ec4
Dylan Burton <dylan@dran-mc.com>
2017-06-22 12:10:54 -0600

    Initial Commmit

---
+++ ChessGame/src/me/dylanburton/chess/ScoreView.java
@@ -0,0 +1,5 @@
+package me.dylanburton.chess;
+
+public class ScoreView {
+
+}
---
+++ ChessGame/src/me/dylanburton/chess/White.java
@@ -0,0 +1,20 @@
+package me.dylanburton.chess;
+
+public enum White {
+	PAWN(1),
+    BISHOP(4),
+    KNIGHT(3),
+    ROOK(5),
+	QUEEN(9),
+	KING(10);
+
+    private final int value;
+
+    White(int value) {
+        this.value = value;
+    }
+    int value() {
+        return this.value;
+    }
+
+}
